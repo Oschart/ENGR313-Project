029d954624989adc865ac0f034345d3023485e2b
Mahmoud363 <38327235+Mahmoud363@users.noreply.github.com>
2019-12-10 22:12:37 +0200

    changed the function to take x,y

--- numerical_int.m
+++ numerical_int.m
@@ -1,46 +1,17 @@
-function [res] = numerical_int( splines,values,x1, x2)
+function [res] = numerical_int( x, y)
 % this function uses the trapizoidal method to calculate the value of the
 % integral
-    n=length(values);
-    flag = 0;
-    h = (x2-x1)/n;
+    n = length(x);
     sum =0;
-    for i=1:n-1
-        x = x1+i*h;
-        idx = get_interval(values,x);
-        if(idx~=-1)
-            spline = splines(idx,:);
-            sum = sum +F(spline,x );
-        
-        else   % handle out of range
-            flag=1;
-            break;
-        end
+    for i=2:n-1
+        h= (x(i+1)-x(i))/2;
+        sum = sum + h*(y(i)+y(i+1));
+    end
+    h1= (x(2)-x(1))/2;
+    val1= h1*(y(1)+y(2));
+    hn= (x(n)-x(n-1))/2;
+    valn= hn*(y(n)+y(n-1));
+    res = val1+valn+2*sum;
     
-    end
-    if(flag==0)
-        idx = get_interval(values,x1);
-        if(idx~=-1)
-            spline = splines(idx,:);
-            y1 = F(spline,x1 );
-
-            idx = get_interval(values,x2);
-            if(idx~=-1)
-            spline = splines(idx,:);
-            y2 = F(spline,x2 );
-
-            res = (h/2)*(y1+y2+2*sum);
-            else
-                flag =1;
-            end
-        else 
-            flag=1;
-        end
-    
-    end
-    if(flag==1)
-        fprintf("Cannot integrate, will return -1");
-        res=-1;
-    end
 
 end
